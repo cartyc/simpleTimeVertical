076711a14ca50d0913b05b6ae8d454750a3e594c
Chris <chris.carty@gmail.com>
2015-04-09 20:59:18 -0400

    added degree symbol

--- src/main.c
+++ src/main.c
@@ -174,7 +174,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   while(t != NULL){
     switch(t->key){
       case KEY_TEMPERATURE:
-        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%d°C", (int)t->value->int32);
         break;
       case KEY_CONDITIONS:
         snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
